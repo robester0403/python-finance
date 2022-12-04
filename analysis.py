cd6b1bdd83cc187ebe3fdbc07acdf8631e0427c4
robester0403 <robkso@gmail.com>
2022-12-03 23:50:51 -0500

    Small changes to the BSM toolkit

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import streamlit as st
 
 
 load_dotenv(".env")
@@ -14,19 +15,20 @@ years = 30
 
 income_statement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{company}?limit={years}&apikey={API_KEY}").json()
 
-print(income_statement)
 # next step, use pandas to get take JSON object and maake it into a dataframe
 
 # assign to revenues an array of the revenues from the income statement
 # use the for loop to itrate over the length of the income statement array  
-# revenues = list(reversed([ income_statement[i]['revenue'] for i in range(len(income_statement))]))
-# profits = list(reversed([income_statement[i]['grossProfit'] for i in range(len(income_statement))]))
-
-# plt.plot(revenues, label="revenues")
-# plt.plot(profits, label="profits")
-# plt.title(f"{company} revenues and profits")
-# plt.legend(loc="upper left")
-# plt.show()
+revenues = list(reversed([ income_statement[i]['revenue'] for i in range(len(income_statement))]))
+profits = list(reversed([income_statement[i]['grossProfit'] for i in range(len(income_statement))]))
+# st.line_chart(revenues)
+# st.line_chart(profits)
+st.dataframe(revenues)
+plt.plot(revenues, label="revenues")
+plt.plot(profits, label="profits")
+plt.title(f"{company} revenues and profits")
+plt.legend(loc="upper left")
+plt.show()
 
 # income_statementcsv = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{company}?datatype=csv&limit={years}&apikey={API_KEY}")
 
--- bsm.py
+++ bsm.py
@@ -1,6 +1,9 @@
 # Black scholes model calculations
 import numpy as np
 from scipy.stats import norm
+from py_vollib.black_scholes import black_scholes as bs
+from py_vollib.black_scholes.greeks.analytical import delta, gamma, vega, theta, rho
+
 
 r = 0.05 # interest rate
 S = 30 # Underlying stock price
@@ -8,16 +11,84 @@ K = 40 # Strike price
 T = 240/365 # Time to maturity
 sigma = 0.3 # Volatility
 
+# pricing call or put option
 def blackScholes(r, S, K, T, sigma, type="C"): # this function returns the price of a call or put option
   d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
   d2 = d1 - sigma*np.sqrt(T) # d2 is the same as d1 but with a different value
   try:
-    if type == "C":
-      price = S*norm.cdf(d1, 0 , 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1) # cdf is the cumulative distribution function
-    elif type == "P":
-      price = K*np.exp(-r*T)*norm.cdf(-d2,0,1) - S*norm.cdf(-d1,0 ,1)
+    if type == "c":
+      price = S*norm.cdf(d1, 0, 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+    elif type == "p":
+      price = K*np.exp(-r*T)*norm.cdf(-d2, 0, 1) - S*norm.cdf(-d1, 0, 1)
     return price
   except:
-      print("Error")
-print('Option price is:')
-print(round(blackScholes(r, S, K, T, sigma, type="C"), 2))
+      print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+# delta calculation
+def delta_calc(r, S, K, T, sigma, type="c"):
+  "Calculate delta of an option"
+  d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+  try:
+    if type == "c":
+      delta_calc = norm.cdf(d1, 0, 1)
+    elif type == "p":
+      delta_calc = -norm.cdf(-d1, 0, 1)
+    return delta_calc, delta(type, S, K, T, r, sigma)
+  except:
+      print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def gamma_calc(r, S, K, T, sigma, type="c"):
+  "Calculate gamma of a option"
+  d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+  d2 = d1 - sigma*np.sqrt(T)
+  try:
+    gamma_calc = norm.pdf(d1, 0, 1)/(S*sigma*np.sqrt(T))
+    return gamma_calc, gamma(type, S, K, T, r, sigma)
+  except:
+    print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+
+def vega_calc(r, S, K, T, sigma, type="c"):
+  "Calculate BS price of call/put"
+  d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+  d2 = d1 - sigma*np.sqrt(T)
+  try:
+    vega_calc = S*norm.pdf(d1, 0, 1)*np.sqrt(T)
+    return vega_calc*0.01, vega(type, S, K, T, r, sigma)
+  except:
+      print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def theta_calc(r, S, K, T, sigma, type="c"):
+  "Calculate BS price of call/put"
+  d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+  d2 = d1 - sigma*np.sqrt(T)
+  try:
+    if type == "c":
+      theta_calc = -S*norm.pdf(d1, 0, 1)*sigma/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+    elif type == "p":
+      theta_calc = -S*norm.pdf(d1, 0, 1)*sigma/(2*np.sqrt(T)) + r*K*np.exp(-r*T)*norm.cdf(-d2, 0, 1)
+    return theta_calc/365, theta(type, S, K, T, r, sigma)
+  except:
+    print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def rho_calc(r, S, K, T, sigma, type="c"):
+  "Calculate BS price of call/put"
+  d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+  d2 = d1 - sigma*np.sqrt(T)
+  try:
+    if type == "c":
+      rho_calc = K*T*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+    elif type == "p":
+      rho_calc = -K*T*np.exp(-r*T)*norm.cdf(-d2, 0, 1)
+    return rho_calc*0.01, rho(type, S, K, T, r, sigma)
+  except:
+      print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+option_type='p'
+print("Option Price: ", round(blackScholes(r, S, K, T, sigma, option_type),3))
+print("       Delta: ", [round(x,3) for x in delta_calc(r, S, K, T, sigma, option_type)])
+print("       Gamma: ", [round(x,3) for x in gamma_calc(r, S, K, T, sigma, option_type)])
+print("       Vega : ", [round(x,3) for x in vega_calc(r, S, K, T, sigma, option_type)])
+print("       Theta: ", [round(x,3) for x in theta_calc(r, S, K, T, sigma, option_type)])
+print("       Rho  : ", [round(x,3) for x in rho_calc(r, S, K, T, sigma, option_type)])
+
