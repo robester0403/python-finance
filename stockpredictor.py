cf84e3c2cb7fb8c1ed022ff024f6c73467d82c55
robester0403 <robkso@gmail.com>
2022-12-04 21:32:41 -0500

    change data frame to have a new column as Prophet cannot read the index

--- stockpredictor.py
+++ stockpredictor.py
@@ -1,10 +1,18 @@
 import pandas as pd
 from prophet import Prophet
+import pandas_datareader as web
+import datetime as dt
 
-data = pd.read_csv("stockdata.csv")
+stock_ticker = "AAPL"
+start = dt.datetime(2020,1,1 )
+end = dt.datetime.now()
+
+data = web.DataReader(stock_ticker, "yahoo", start, end)
+data['Date'] = data.index
 data= data[["Date", "Close"]]
 
 data.columns = ["ds", "y"]
+print(data.head())
 
 prophet = Prophet(daily_seasonality=True) 
 prophet.fit(data)
